0250845a723fef8fcc8074ae537e73c86344cc64
pdonimantha <76107175+pdonimantha@users.noreply.github.com>
2020-12-30 14:00:58 +0530

    Update sql.php
    update sql.php file

--- sql.php
+++ sql.php
@@ -1,8 +1,95 @@
-- -------------------------------------------------------- -- -- Table structure for table `credentials` -- CREATE TABLE `credentials` ( `email` varchar(150) NOT NULL, `password` varchar(300) NOT NULL, `status` varchar(10) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 29, 2020 at 12:15 AM
+-- Server version: 8.0.22-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- -------------------------------------------------------- -- -- Table structure for table `users` -- CREATE TABLE `users` ( `email` varchar(150) NOT NULL, `title` varchar(10) NOT NULL, `f_name` varchar(50) NOT NULL, `l_name` varchar(50) NOT NULL, `add_line_1` varchar(100) NOT NULL, `add_line_2` varchar(100) NOT NULL, `city` varchar(50) NOT NULL, `zipcode` varchar(10) NOT NULL, `country` varchar(25) NOT NULL, `phone` varchar(15) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+--
+-- Database: `sea_side`
+--
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `credentials`
+--
 
+CREATE TABLE `credentials` (
+  `email` varchar(150) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(150) NOT NULL,
+  `title` varchar(10) NOT NULL,
+  `f_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `add_line_1` varchar(100) NOT NULL,
+  `add_line_2` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `country` varchar(25) NOT NULL,
+  `phone` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `title`, `f_name`, `l_name`, `add_line_1`, `add_line_2`, `city`, `zipcode`, `country`, `phone`) VALUES
+('abc@gmail.com', 'Prof', 'Abc', 'Def', 'No 44', 'Colombo Road', 'S', '61000', 'Sri Lanka', '+0'),
+('qaz@gmail.com', 'Mr', 'q', 'q', 'No 44', 'Colombo Road', 'A', '61', 'Sri Lanka', '+00'),
+('root@123', 'Mr', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_no`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
